6853755ee8eebc25e0522e80eca986cf534ab29d
Adrian <matchadev22@gmail.com>
2025-08-18 16:09:14 +1200

    Completes removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...removeValue) {
+    return arr.filter((n) => !removeValue.includes(n));
+ };
+ 
 
 // Do not edit below this line
 module.exports = removeFromArray;
