2569c915e445fdac22bca45a40742b5cf7ed22d4
Adrian <matchadev22@gmail.com>
2025-08-19 14:07:04 +1200

    Completes sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let finalsum = 0;
 
+    if(firstNum > lastNum){
+       let temp = firstNum;
+        firstNum = lastNum;
+        lastNum = temp;
+    }else if(firstNum <= 0){
+        return "ERROR";
+    }else if(!Number.isInteger(lastNum) || !Number.isInteger(firstNum)){
+        return "ERROR";
+    }
+    for(let i = firstNum; i < lastNum + 1 ; i++){
+        finalsum = finalsum + i
+      }
+    return finalsum;
 };
 
 // Do not edit below this line
